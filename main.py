832404ba3dcaa43fde06c3dc8b2a648820689d83
GerbrUh <106966673+GerbrUh@users.noreply.github.com>
2026-03-12 16:27:14 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -5,23 +5,57 @@ from datastores.sql_store import engine
 SQLModel.metadata.drop_all(engine)
 SQLModel.metadata.create_all(engine)
 
-user = User(name="Adriaan", email="adriaan@example.com")
+User.purge_data()
+Address.purge_data()
+Information.purge_data()
+
+user = User(id='1', name="Max", email="max@example.com", age=28)
 address = Address(street="Main Street", city="Eindhoven")
 info = Information(Key='1', email="Example@Mail.com", phone_number="+31 1234567")
 
-
+print("\nSave")
 user.save()
 
-fetched = User.find_by_id(user.id)
-print(f"Fetched: {fetched}")
+data = [
+    {"id": "2", "name": "Alex", "email":"alex@example.com", "age": 29},
+    {"id": "3", "name": "Charles", "email": "charles@example.com", "age": 28}
+]
+users = User.mass_save(data)
 
-fetched.name = "Updated Name"
-fetched.save()
+print(User.find_all())
 
-all_users = User.find_all()
-for u in all_users:
-    print(u)
+print("\nFind")
+print(User.find_by_id("2"))
+print(User.find_by_field({"name": "Alex"}))
+print(User.find_by_field({"email": {"$sub": "max"}}))
+print(User.find_by_field({"age": {"$gte": 28, "$lt": 29}}))
 
-fetched.delete()
+User.save_to_json('data')
 
-info.save()
+print("\nDelete")
+user.delete()
+User.mass_delete(["2", "3"])
+print(User.find_all())
+
+print("\nJson")
+User.load_from_json('data')
+print(User.find_all())
+
+print("\nRA")
+rename = RA.Rename(User, {'name': 'u-name', 'email': 'u-email', 'age': 'u-age'})
+print("Rename: " + str(rename))
+projection = RA.Projection(rename, ['u-name', 'u-email', 'u-age'])
+print("Projection: " + str(projection))
+print("Cartesian Join: " + str(RA.Join(User, projection)))
+print("Match Join: " + str(RA.Join(User, rename)))
+
+select = RA.Select(User, {'age': {'$lt': 29}})
+print("Select: " + str(select))
+union = RA.Union_op(RA.Select(User, {'name': 'Max'}), RA.Select(User, {'name': 'Alex'}))
+print("Union: " + str(union))
+print("Difference: " + str(RA.Difference(select, union)))
+print("Intersection: " + str(RA.Intersect(select, union)))
+
+union2 = RA.Union_op(User, User)
+print("Order: " + str(RA.Order(union2, ['age'])))
+print("Distinct: " + str(RA.Distinct(RA.Order(union2, ['age']))))
